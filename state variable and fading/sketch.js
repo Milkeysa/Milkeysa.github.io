599668ee9c5a53c85bfc3d812a32cc269234ba41
Milkeysa <mohamedmilkey@gmail.com>
2023-09-22 14:31:01 -0600

    Update sketch.js

--- state variable and fading/sketch.js
+++ state variable and fading/sketch.js
@@ -1,15 +1,23 @@
-// Project Title
-// Your Name
+// State Variables And Fading
+// Milkeysa Mohamed Jamal
 // Date
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// a look at state variables.
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
+function renderSquare(){
+  //draw 2 rectangles on screen
+  rect(0,0,width/2,height);
+  rect(midth,2,0,width/2,height);
+}
+
 function draw() {
   background(220);
+  renderSquare();
 }
+
